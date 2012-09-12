80e9670ed202445f79b732f2c2fec77458381d20
Jeremy Keiper <jeremy@openmrs.org>
2012-09-12 14:36:57 -0400

    added basic service test file with no real tests in it

---
+++ api/src/test/java/org/openmrs/module/amrsreport/service/MohCoreServiceTest.java
@@ -0,0 +1,38 @@
+package org.openmrs.module.amrsreport.service;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MohCoreServiceTest {
+
+	public MohCoreServiceTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Sample test just to have something here
+	 */
+	@Test
+	public void testFoo() {
+		// pass
+	}
+
+}
