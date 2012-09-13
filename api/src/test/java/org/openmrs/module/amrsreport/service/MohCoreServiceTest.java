012ffec1e18ae1d3a504b93d2764a0041810d0d6
ojwanganto <ojwangwachiaje@gmail.com>
2012-09-13 14:37:25 +0300

    Changes made on REPORTS-31

--- api/src/test/java/org/openmrs/module/amrsreport/service/MohCoreServiceTest.java
+++ api/src/test/java/org/openmrs/module/amrsreport/service/MohCoreServiceTest.java
@@ -5,8 +5,16 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.amrsreport.userlocation.UserLocation;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.User;
+import org.openmrs.Location;
 
-public class MohCoreServiceTest {
+import src.test.java.org.openmrs.module.dataintegrity.DataIntegrityService;
+
+public class MohCoreServiceTest extends BaseModuleContextSensitiveTest{
 
 	public MohCoreServiceTest() {
 	}
@@ -33,6 +41,57 @@ public class MohCoreServiceTest {
 	@Test
 	public void testFoo() {
 		// pass
+	}
+
+	/**
+	 * @see MohCoreService#getUserLocation(Integer)
+	 * @verifies get a UserLocation by its Id
+	 */
+	@Test
+	public void getUserLocation_shouldGetAUserLocationByItsId()
+			throws Exception {
+		//TODO auto-generated
+		Assert.fail("Not yet implemented");
+	}
+
+	/**
+	 * @see MohCoreService#purgeUserLocation(UserLocation)
+	 * @verifies purge a UserLocation
+	 */
+	@Test
+	public void purgeUserLocation_shouldPurgeAUserLocation() throws Exception {
+		//TODO auto-generated
+		Assert.fail("Not yet implemented");
+	}
+
+	/**
+	 * @see MohCoreService#saveUserLocation(UserLocation)
+	 * @verifies save a UserLocation
+	 */
+	@Test
+	public void saveUserLocation_shouldSaveAUserLocation() throws Exception {
+		
+		//Create an instance of the service object
+		
+		MohCoreService cservice = Context.getService(MohCoreService.class);
+		
+		//Create a sample User 
+		User sysUser = new User();
+		sysUser.setUserId(2);
+		
+		//Create a sample Location
+		Location userLoc = new Location();
+		userLoc.setLocationId(3);
+		
+		//create an instance of UserLocation class
+		UserLocation userlocation = new UserLocation();
+		userlocation.setUserLocationId(3);
+		userlocation.setSysUser(sysUser);
+		userlocation.setUserLoc(userLoc);
+		
+		cservice.saveUserLocation(userlocation);
+		
+		
 	}
 
 }
