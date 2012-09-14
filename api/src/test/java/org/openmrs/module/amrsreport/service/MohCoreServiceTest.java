e4ac5384454f9ed7d6f0113e4a91d47c36693764
ojwanganto <antony@antony-HP-G62-Notebook-PC.(none)>
2012-09-14 12:52:15 +0300

    Changes made on REPORTS-31: Changes on test methods

--- api/src/test/java/org/openmrs/module/amrsreport/service/MohCoreServiceTest.java
+++ api/src/test/java/org/openmrs/module/amrsreport/service/MohCoreServiceTest.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.amrsreport.service;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -50,8 +52,30 @@ public class MohCoreServiceTest extends BaseModuleContextSensitiveTest{
 	@Test
 	public void getUserLocation_shouldGetAUserLocationByItsId()
 			throws Exception {
-		//TODO auto-generated
-		Assert.fail("Not yet implemented");
+		//Create an instance of the service object
+		
+		MohCoreService cservice = Context.getService(MohCoreService.class);
+		
+		//Create a sample User 
+		User sysUser = new User();
+		sysUser.setUserId(501);
+		
+		//Create a sample Location -- using New Location as an example
+		Location userLoc = new Location();
+		userLoc.setLocationId(1);
+		
+		//create an instance of UserLocation class
+		UserLocation userlocation = new UserLocation();
+		userlocation.setSysUser(sysUser);
+		userlocation.setUserLoc(userLoc);
+		//setting returnedUserLocation to hold the returned UserLocation object after save process
+		UserLocation returnedUserLocation=cservice.saveUserLocation(userlocation);
+		
+		//returnedUserLocation.getUserLocationId() will hold the id of the saved object
+		//cservice.getUserLocation(returnedUserLocation.getUserLocationId()) should return an object with the specidied ID
+		//The test should not return null object
+		Assert.assertNotNull(cservice.getUserLocation(returnedUserLocation.getUserLocationId()));
+		
 	}
 
 	/**
@@ -60,8 +84,41 @@ public class MohCoreServiceTest extends BaseModuleContextSensitiveTest{
 	 */
 	@Test
 	public void purgeUserLocation_shouldPurgeAUserLocation() throws Exception {
-		//TODO auto-generated
-		Assert.fail("Not yet implemented");
+		
+		//Create an instance of the service object
+		
+		MohCoreService cservice = Context.getService(MohCoreService.class);
+		
+		//Create a sample User 
+		User sysUser = new User();
+		sysUser.setUserId(501);
+		
+		//Create a sample Location -- using New Location
+		Location userLoc = new Location();
+		userLoc.setLocationId(1);
+		
+		//create an instance of UserLocation class
+		UserLocation userlocation = new UserLocation();
+		userlocation.setSysUser(sysUser);
+		userlocation.setUserLoc(userLoc);
+		//sets up uloc variable to hold the saved UserLocation object
+		UserLocation uloc = cservice.saveUserLocation(userlocation);
+		
+		//uloc.getUserLocationId() should return the id of the saved object in the database
+		//Assert.assertNotNull(uloc.getUserLocationId()) should never be null if the object was saved successfully
+		Integer userlocationid=uloc.getUserLocationId();
+		Assert.assertNotNull(userlocationid);
+		
+		//If Assert.assertNotNull(uloc.getUserLocationId()); passes, we purge the uloc object from the database
+		cservice.purgeUserLocation(uloc);
+		
+		/*cservice.getUserLocation(userlocationid);*/
+		//Checks to see that the object has been purged from the database
+		Assert.assertNull(cservice.getUserLocation(userlocationid));
+		
+		
+		
+		
 	}
 
 	/**
@@ -77,19 +134,27 @@ public class MohCoreServiceTest extends BaseModuleContextSensitiveTest{
 		
 		//Create a sample User 
 		User sysUser = new User();
-		sysUser.setUserId(2);
+		sysUser.setUserId(501);
 		
-		//Create a sample Location
+		//Create a sample Location -- using New Location
 		Location userLoc = new Location();
-		userLoc.setLocationId(3);
+		userLoc.setLocationId(1);
 		
 		//create an instance of UserLocation class
 		UserLocation userlocation = new UserLocation();
-		userlocation.setUserLocationId(3);
 		userlocation.setSysUser(sysUser);
 		userlocation.setUserLoc(userLoc);
 		
-		cservice.saveUserLocation(userlocation);
+		
+		//Checks to see that getUserLocationId() returns null before save
+		Assert.assertNull(userlocation.getUserLocationId());
+		
+		//sets up uloc variable to hold the returned object after save operation
+		UserLocation uloc=cservice.saveUserLocation(userlocation);
+		
+		//Checks to ascertain that getUserLocationId() of the returned object is never null
+		Assert.assertNotNull(uloc.getUserLocationId());
+		
 		
 		
 	}
