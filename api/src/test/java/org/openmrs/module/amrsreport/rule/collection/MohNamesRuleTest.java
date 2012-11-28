8c22310501391276df368f5e73866b7d6fc48746
ojwanganto <ojwangwachiaje@gmail.com>
2012-11-28 15:02:13 +0300

    Added a second test for MohNamesRule class

--- api/src/test/java/org/openmrs/module/amrsreport/rule/collection/MohNamesRuleTest.java
+++ api/src/test/java/org/openmrs/module/amrsreport/rule/collection/MohNamesRuleTest.java
@@ -2,21 +2,29 @@ package org.openmrs.module.amrsreport.rule.collection;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openmrs.*;
+import org.openmrs.Location;
 import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.PatientIdentifierType;
 import org.openmrs.Person;
+import org.openmrs.api.EncounterService;
+import org.openmrs.api.PatientService;
 import org.openmrs.module.amrsreport.rule.collection.MohNamesRule;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.openmrs.api.context.Context;
 
 import org.openmrs.logic.result.Result;
 import java.lang.String;
+import java.util.Date;
 
 /**
  *Test file for MohNamesRule class
  */
 public class MohNamesRuleTest extends BaseModuleContextSensitiveTest {
     /**
-     * @verifies return the full name of a patient
+     * @should return full names of a patient
+     * @verifies that full name of a patient is returned
      * @see MohNamesRule#evaluate(org.openmrs.logic.LogicContext, Integer, java.util.Map)
      */
     @Test
@@ -33,6 +41,33 @@ public class MohNamesRuleTest extends BaseModuleContextSensitiveTest {
 
         Assert.assertEquals("A different name was returned for the patient",expected,actual);
 
+
+
+
+    }
+
+    /**
+     * @should return two names for the patient
+     * @verifies that only two names are returned
+     * @see MohNamesRule#evaluate(org.openmrs.logic.LogicContext, Integer, java.util.Map)
+     */
+    @Test
+    public void evaluate_shouldReturnTwoNames() throws Exception {
+
+        Patient patient = new Patient();
+        patient.setPersonId(501);
+
+        Assert.assertNotNull("The patient is null",patient);
+
+        PatientIdentifierType pit = new PatientIdentifierType(2);
+
+        Location location = new Location(2);
+        PatientIdentifier pi = new PatientIdentifier("234", pit, location);
+
+
+        pi.setPatient(patient);
+        PatientService ps = Context.getPatientService();
+        Patient savedPatient =ps.savePatient(patient);
 
 
 
