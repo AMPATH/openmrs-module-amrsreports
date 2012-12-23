8ba6815fd3b9c4552408e1603a0465f79f9f3b3c
Jeremy Keiper <jeremy@openmrs.org>
2012-12-22 22:59:17 -0500

    modified rule and added tests for MOH Flucanozole start/stop dates

--- api/src/main/java/org/openmrs/module/amrsreport/rule/medication/MohFluconazoleStartStopDateRule.java
+++ api/src/main/java/org/openmrs/module/amrsreport/rule/medication/MohFluconazoleStartStopDateRule.java
@@ -1,42 +1,30 @@
 package org.openmrs.module.amrsreport.rule.medication;
 
-import org.openmrs.Concept;
 import org.openmrs.Obs;
 import org.openmrs.OpenmrsObject;
-import org.openmrs.api.context.Context;
 import org.openmrs.logic.LogicContext;
 import org.openmrs.logic.LogicException;
 import org.openmrs.logic.result.Result;
-import org.openmrs.logic.result.Result.Datatype;
-import org.openmrs.logic.rule.RuleParameterInfo;
 import org.openmrs.module.amrsreport.cache.MohCacheUtils;
 import org.openmrs.module.amrsreport.rule.MohEvaluableNameConstants;
-import org.openmrs.module.amrsreport.service.MohCoreService;
-import org.openmrs.module.amrsreport.util.MohFetchOrdering;
-import org.openmrs.module.amrsreport.util.MohFetchRestriction;
-import org.openmrs.util.OpenmrsUtil;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
-import java.util.Set;
 
 public class MohFluconazoleStartStopDateRule extends DrugStartStopDateRule {
 
-	public static final String TOKEN = "MOH Fluconazole Start-Stop Date";
+	public MohFluconazoleStartStopDateRule() {
 
-	private static final Concept StartDrugs = MohCacheUtils.getConcept(MohEvaluableNameConstants.START_DRUGS);
-	private static final Concept FluconazoleDrug = MohCacheUtils.getConcept(MohEvaluableNameConstants.FLUCONAZOLE);
-	private static final Concept StopDrugs = MohCacheUtils.getConcept(MohEvaluableNameConstants.STOP_ALL);
+		this.startConcepts = Arrays.<OpenmrsObject>asList(
+				MohCacheUtils.getConcept(MohEvaluableNameConstants.CRYPTOCOCCAL_TREATMENT_PLAN),
+				MohCacheUtils.getConcept(MohEvaluableNameConstants.CRYPTOCOSSUS_TREATMENT_STARTED)
+		);
 
-	private static final List<OpenmrsObject> questionConcepts = Arrays.<OpenmrsObject>asList(
-			new Concept[]{
-					MohCacheUtils.getConcept(MohEvaluableNameConstants.CRYPTOCOCCAL_TREATMENT_PLAN),
-					MohCacheUtils.getConcept(MohEvaluableNameConstants.CRYPTOCOSSUS_TREATMENT_STARTED)
-			});
+		this.stopConcepts = Collections.singletonList((OpenmrsObject) MohCacheUtils.getConcept(MohEvaluableNameConstants.CRYPTOCOCCAL_TREATMENT_PLAN));
+
+		this.TOKEN = "MOH Fluconazole Start-Stop Date";
+	}
 
 	/**
 	 * MOH Fluconazole Start-Stop Date
@@ -45,71 +33,30 @@ public class MohFluconazoleStartStopDateRule extends DrugStartStopDateRule {
 	 * CRYPTOCOSSUS TREATMENT STARTED (1278)= FLUCONAZOLE (747) were captured.
 	 * <p/>
 	 * For Fluconazole stopdate: Use the observation date when CRYPTOCOCCAL TREATMENT PLAN (1277)=STOP ALL (1260) was captured
+	 *
+	 * @should start on CRYPTOCOCCAL_TREATMENT_PLAN is START_DRUGS
+	 * @should start on CRYPTOCOSSUS_TREATMENT_STARTED is FLUCONAZOLE
+	 * @should stop on CRYPTOCOCCAL_TREATMENT_PLAN is STOP_ALL
+	 * @should start and stop on CRYPTOCOCCAL_TREATMENT_PLAN with correct values
 	 */
 	@Override
-	protected Result evaluate(LogicContext context, Integer patientId, Map<String, Object> parameters) throws LogicException {
-		// set up query for observations in order by ascending date
-		Map<String, Collection<OpenmrsObject>> restrictions = new HashMap<String, Collection<OpenmrsObject>>();
-		restrictions.put("concept", questionConcepts);
-		MohFetchRestriction fetchRestriction = new MohFetchRestriction();
-		fetchRestriction.setFetchOrdering(MohFetchOrdering.ORDER_ASCENDING);
-
-		// get the start observations
-		List<Obs> observations = Context.getService(MohCoreService.class).getPatientObservations(patientId, restrictions, fetchRestriction);
-
-		List<Obs> startObs = new ArrayList<Obs>();
-		List<Obs> stopObs = new ArrayList<Obs>();
-
-		// find start and stop observations
-		for (Obs obs : observations) {
-			if (OpenmrsUtil.nullSafeEquals(StartDrugs, obs.getValueCoded()) || OpenmrsUtil.nullSafeEquals(FluconazoleDrug, obs.getValueCoded())) {
-				startObs.add(obs);
-			} else if (OpenmrsUtil.nullSafeEquals(StopDrugs, obs.getValueCoded())) {
-				stopObs.add(obs);
-			}
-		}
-		return buildResultFromObservations(startObs, stopObs);
+	public Result evaluate(final LogicContext context, final Integer patientId, final Map<String, Object> parameters) throws LogicException {
+		return this.getResult(patientId);
 	}
 
 	@Override
 	protected boolean validateStartObs(Obs obs) {
-		return true;
+		return (compareConceptToName(obs.getConcept(), MohEvaluableNameConstants.CRYPTOCOCCAL_TREATMENT_PLAN) &&
+				compareConceptToName(obs.getValueCoded(), MohEvaluableNameConstants.START_DRUGS))
+				||
+				(compareConceptToName(obs.getConcept(), MohEvaluableNameConstants.CRYPTOCOSSUS_TREATMENT_STARTED) &&
+						compareConceptToName(obs.getValueCoded(), MohEvaluableNameConstants.FLUCONAZOLE));
 	}
 
 	@Override
 	protected boolean validateStopObs(Obs obs) {
-		return true;
-	}
-
-	protected String getEvaluableToken() {
-		return TOKEN;
-	}
-
-	/**
-	 * @see org.openmrs.logic.Rule#getDependencies()
-	 */
-	@Override
-	public String[] getDependencies() {
-		return new String[]{};
-	}
-
-	/**
-	 * Get the definition of each parameter that should be passed to this rule execution
-	 *
-	 * @return all parameter that applicable for each rule execution
-	 */
-	@Override
-	public Datatype getDefaultDatatype() {
-		return Datatype.TEXT;
-	}
-
-	public Set<RuleParameterInfo> getParameterList() {
-		return null;
-	}
-
-	@Override
-	public int getTTL() {
-		return 0;
+		return compareConceptToName(obs.getConcept(), MohEvaluableNameConstants.CRYPTOCOCCAL_TREATMENT_PLAN) &&
+				compareConceptToName(obs.getValueCoded(), MohEvaluableNameConstants.STOP_ALL);
 	}
 
 }
