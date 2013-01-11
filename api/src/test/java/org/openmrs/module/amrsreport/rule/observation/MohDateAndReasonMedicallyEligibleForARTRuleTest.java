87d54b4c2fca3e0e44a173642bd3e3e88ed94bb5
ojwanganto <ojwangwachiaje@gmail.com>
2013-01-11 16:31:23 +0300

    Setup for Mockito tests for the class

--- api/src/test/java/org/openmrs/module/amrsreport/rule/observation/MohDateAndReasonMedicallyEligibleForARTRuleTest.java
+++ api/src/test/java/org/openmrs/module/amrsreport/rule/observation/MohDateAndReasonMedicallyEligibleForARTRuleTest.java
@@ -1,46 +1,113 @@
 package org.openmrs.module.amrsreport.rule.observation;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.openmrs.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.openmrs.Concept;
-import org.openmrs.ConceptAnswer;
-import org.openmrs.ConceptName;
-import org.openmrs.Encounter;
 import org.openmrs.EncounterType;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
-import org.openmrs.PatientIdentifier;
-import org.openmrs.PatientIdentifierType;
-import org.openmrs.User;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.ObsService;
-import org.openmrs.api.PatientSetService;
+import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
-import org.openmrs.logic.LogicContext;
 import org.openmrs.logic.result.Result;
-import org.openmrs.module.amrsreport.rule.observation.MohDateAndReasonMedicallyEligibleForARTRule;
-import org.openmrs.module.amrsreport.rule.util.MohTestUtils;
-import org.openmrs.test.BaseModuleContextSensitiveTest;
-
-import java.lang.Integer;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
-
+import org.openmrs.module.amrsreport.rule.MohEvaluableConstants;
 import org.openmrs.module.amrsreport.rule.MohEvaluableNameConstants;
+import org.openmrs.module.amrsreport.rule.observation.MohDateAndReasonMedicallyEligibleForARTRule;
+import org.openmrs.module.amrsreport.util.MohFetchRestriction;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
- * Created with IntelliJ IDEA.
- * User: oliver
- * Date: 11/12/12
- * Time: 11:36 AM
- * To change this template use File | Settings | File Templates.
+ * Test file for MohDateAndReasonMedicallyEligibleForARTRule.
  */
-public class MohDateAndReasonMedicallyEligibleForARTRuleTest extends BaseModuleContextSensitiveTest {
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Context.class)
+public class MohDateAndReasonMedicallyEligibleForARTRuleTest{
 
-	ConceptService conceptService = Context.getConceptService();
+    private static final List<String> initConcepts = Arrays.asList(
+            MohDateAndReasonMedicallyEligibleForARTRule.REASON_CLINICAL,
+            MohDateAndReasonMedicallyEligibleForARTRule.REASON_CLINICAL_CD4,
+            MohDateAndReasonMedicallyEligibleForARTRule.REASON_CLINICAL_CD4_HIV_DNA_PCR,
+            MohDateAndReasonMedicallyEligibleForARTRule.REASON_CLINICAL_HIV_DNA_PCR,
+            MohEvaluableNameConstants.WHO_STAGE_1_ADULT,
+            MohEvaluableNameConstants.WHO_STAGE_2_ADULT,
+            MohEvaluableNameConstants.WHO_STAGE_3_ADULT,
+            MohEvaluableNameConstants.WHO_STAGE_1_PEDS,
+            MohEvaluableNameConstants.WHO_STAGE_3_PEDS,
+            MohEvaluableNameConstants.WHO_STAGE_ADULT,
+            MohEvaluableNameConstants.WHO_STAGE_PEDS,
+            MohEvaluableNameConstants.HIV_DNA_PCR
+    );
+
+    private static final int PATIENT_ID = 5;
+    private Patient patient;
+    private ConceptService conceptService;
+    private PatientService patientService;
+    private ObsService obsService;
+    private MohDateAndReasonMedicallyEligibleForARTRule rule;
+    private List<Obs> currentObs;
+
+    @Before
+    public void setup() {
+
+        // initialize the current obs
+        currentObs = new ArrayList<Obs>();
+
+        patient = new Patient();
+        // build the concept service
+        int i = 0;
+        conceptService = Mockito.mock(ConceptService.class);
+        patientService = Mockito.mock(PatientService.class);
+
+
+        for (String conceptName : initConcepts) {
+            Mockito.when(conceptService.getConcept(conceptName)).thenReturn(new Concept(i++));
+        }
+        Mockito.when(conceptService.getConcept((String) null)).thenReturn(null);
+        Mockito.when(patientService.getPatient(PATIENT_ID)).thenReturn(patient);
+
+        obsService = Mockito.mock(ObsService.class);
+       /* List<Obs> obs = Context.getObsService().getObservations(
+                Arrays.asList(new Person[]{patient}), null, getQuestionConcepts(),
+                null, null, null, null, null, null, null, null, false);*/
+
+        Mockito.when(obsService.getObservations(
+                Arrays.asList(patient),
+                Mockito.anyList(),
+                Mockito.anyList(),
+                Mockito.anyList(),
+                Mockito.anyList(),
+                Mockito.anyList(),
+                Mockito.anyList(),
+                Mockito.anyInt(),
+                Mockito.anyInt(),
+                Mockito.any(Date.class),
+                Mockito.any(Date.class),
+                Mockito.anyBoolean()
+                )).thenReturn(currentObs);
+
+
+        // set up Context
+        PowerMockito.mockStatic(Context.class);
+        Mockito.when(Context.getConceptService()).thenReturn(conceptService);
+        Mockito.when(Context.getObsService()).thenReturn(obsService);
+        Mockito.when(Context.getPatientService()).thenReturn(patientService);
+
+        rule = new MohDateAndReasonMedicallyEligibleForARTRule();
+    }
 
 
 	/**
@@ -50,7 +117,7 @@ public class MohDateAndReasonMedicallyEligibleForARTRuleTest extends BaseModuleC
 	@Test
 	public void evaluate_shouldGetTheDateAndReasonForARTEligibility() throws Exception {
 
-		MohTestUtils.createQuestion(MohEvaluableNameConstants.WHO_STAGE_ADULT, new String[]{
+		/*MohTestUtils.createQuestion(MohEvaluableNameConstants.WHO_STAGE_ADULT, new String[]{
 				MohEvaluableNameConstants.WHO_STAGE_1_ADULT,
 				MohEvaluableNameConstants.WHO_STAGE_2_ADULT,
 				MohEvaluableNameConstants.WHO_STAGE_3_ADULT,
@@ -85,7 +152,7 @@ public class MohDateAndReasonMedicallyEligibleForARTRuleTest extends BaseModuleC
 		sampleEncounter.setPatient(patient);
 		sampleEncounter.setEncounterType(service.getEncounterType("ADULTINITIAL"));
 
-		/*ObsService obsService = Context.getObsService();*/
+		*//*ObsService obsService = Context.getObsService();*//*
 
 		Obs obs = new Obs();
 		obs.setConcept(conceptService.getConceptByName(MohEvaluableNameConstants.WHO_STAGE_ADULT));
@@ -98,10 +165,10 @@ public class MohDateAndReasonMedicallyEligibleForARTRuleTest extends BaseModuleC
 		Encounter resEncounter = service.saveEncounter(sampleEncounter);
 		Integer patientID = resEncounter.getPatient().getPersonId();
 
-		/*Checks if the Encounter has been saved*/
+		*//*Checks if the Encounter has been saved*//*
 		Assert.assertNotNull("Encounter is Null", resEncounter);
 
-		/*Checks to find id patient Id is not null*/
+		*//*Checks to find id patient Id is not null*//*
 		Assert.assertNotNull("PatientID is Null", patientID);
 
 
@@ -115,6 +182,6 @@ public class MohDateAndReasonMedicallyEligibleForARTRuleTest extends BaseModuleC
 		Assert.assertNotNull("Evaluate Method returns Null", evalResult);
 
 	}
-
-
+*/
+    }
 }
