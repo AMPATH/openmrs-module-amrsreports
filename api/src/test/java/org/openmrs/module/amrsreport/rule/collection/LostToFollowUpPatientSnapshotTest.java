bc4d6237248521ee325bc8f2a58dca925505d17a
Jeremy Keiper <jeremy@openmrs.org>
2013-01-16 15:21:28 -0500

    LTFU rule and snapshot updates and test fixes

--- api/src/test/java/org/openmrs/module/amrsreport/rule/collection/LostToFollowUpPatientSnapshotTest.java
+++ api/src/test/java/org/openmrs/module/amrsreport/rule/collection/LostToFollowUpPatientSnapshotTest.java
@@ -1,15 +1,17 @@
 package org.openmrs.module.amrsreport.rule.collection;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.openmrs.*;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.EncounterType;
+import org.openmrs.Obs;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.EncounterService;
-import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.amrsreport.rule.MohEvaluableNameConstants;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -20,6 +22,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 
 /**
@@ -31,157 +35,124 @@ import java.util.Locale;
 @PrepareForTest(Context.class)
 public class LostToFollowUpPatientSnapshotTest {
 
-    private static final List<String> initConcepts = Arrays.asList(
-            LostToFollowUpPatientSnapshot.CONCEPT_DATE_OF_DEATH,
-            LostToFollowUpPatientSnapshot.CONCEPT_DECEASED,
-            LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER,
-            LostToFollowUpPatientSnapshot.CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE,
-            LostToFollowUpPatientSnapshot.CONCEPT_AMPATH
+	private static final List<String> initConcepts = Arrays.asList(
+			LostToFollowUpPatientSnapshot.CONCEPT_DATE_OF_DEATH,
+			LostToFollowUpPatientSnapshot.CONCEPT_DEATH_REPORTED_BY,
+			LostToFollowUpPatientSnapshot.CONCEPT_CAUSE_FOR_DEATH,
+			LostToFollowUpPatientSnapshot.CONCEPT_DECEASED,
+			LostToFollowUpPatientSnapshot.CONCEPT_PATIENT_DIED,
+			LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER,
+			LostToFollowUpPatientSnapshot.CONCEPT_AMPATH,
+			LostToFollowUpPatientSnapshot.CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE,
+			MohEvaluableNameConstants.RETURN_VISIT_DATE,
+			"TRUE"
+	);
 
-    );
+	private String DECEASED_ANSWER = "TRUE";
+	private String ENCOUNTER_TYPE_ADULT_INITIAL = "ADULTINITIAL";
+	private String ENCOUNTER_TYPE_ADULT_RETURN = "ADULTRETURN";
+	private String ENCOUNTER_TYPE_DEATH_REPORT = "DEATHREPORT";
 
-    private static final int PATIENT_ID = 5;
-    private Patient patient;
-    private String DECEASED_ANSWER="TRUE";
-    private String ENCOUNTER_TYPE_ADULT_INITIAL="ADULTINITIAL";
-    private String ENCOUNTER_TYPE_ADULT_RETURN="ADULTRETURN";
-    private String ENCOUNTER_TYPE_DEATH_REPORT="DEATHREPORT";
+	private ConceptService conceptService;
+	private EncounterService encounterService;
+	private LostToFollowUpPatientSnapshot rule;
 
-    private ConceptService conceptService;
-    private EncounterService encounterService;
-    private PatientService patientService;
-    private LostToFollowUpPatientSnapshot rule;
+	private Obs currentObs;
+	private Encounter encounter;
 
-    private Obs currentObs;
-    private Encounter encounter;
+	@Before
+	public void setup() {
+		// build the mock services
+		encounterService = Mockito.mock(EncounterService.class);
 
+		Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_INITIAL)).thenReturn(new EncounterType(0));
+		Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_RETURN)).thenReturn(new EncounterType(1));
+		Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_DEATH_REPORT)).thenReturn(new EncounterType(2));
 
-    @Before
-    public void setup() {
+		// build the concept service
+		int i = 0;
+		conceptService = Mockito.mock(ConceptService.class);
+		for (String conceptName : initConcepts) {
+			Mockito.when(conceptService.getConcept(conceptName)).thenReturn(new Concept(i++));
+		}
+		Mockito.when(conceptService.getConcept((String) null)).thenReturn(null);
 
-        // build the patient
-        patient = new Patient();
+		// set up Context
+		PowerMockito.mockStatic(Context.class);
+		Mockito.when(Context.getConceptService()).thenReturn(conceptService);
+		Mockito.when(Context.getEncounterService()).thenReturn(encounterService);
 
-        // build the mock patient service
-        patientService = Mockito.mock(PatientService.class);
-        Mockito.when(patientService.getPatient(PATIENT_ID)).thenReturn(patient);
+		rule = new LostToFollowUpPatientSnapshot();
+	}
 
-        // build the mock  services
-        encounterService = Mockito.mock(EncounterService.class);
+	/**
+	 * generate a date from a string
+	 *
+	 * @param date
+	 * @return
+	 */
+	private Date makeDate(String date) {
+		try {
+			return new SimpleDateFormat("d MMM yyyy", Locale.ENGLISH).parse(date);
+		} catch (Exception e) {
+			// pass
+		}
+		return new Date();
+	}
 
-        Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_INITIAL)).thenReturn(new EncounterType(0));
-        Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_RETURN)).thenReturn(new EncounterType(1));
-        Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_DEATH_REPORT)).thenReturn(new EncounterType(2));
+	/**
+	 * adds an observation with the given date as the obs datetime
+	 *
+	 * @param concept
+	 * @param date
+	 */
+	private void setObs(String concept, String answer, String date) {
+		currentObs = new Obs();
+		currentObs.setConcept(conceptService.getConcept(concept));
+		currentObs.setValueCoded(conceptService.getConcept(answer));
+		currentObs.setObsDatetime(makeDate(date));
+		currentObs.setEncounter(encounter);
+	}
 
-        // build the concept service
-        int i = 0;
-        conceptService = Mockito.mock(ConceptService.class);
-        for (String conceptName : initConcepts) {
-            Mockito.when(conceptService.getConcept(conceptName)).thenReturn(new Concept(i++));
-        }
-        Mockito.when(conceptService.getConcept((this.DECEASED_ANSWER))).thenReturn(new Concept(10));
-        Mockito.when(conceptService.getConcept((String) null)).thenReturn(null);
+	/**
+	 * adds an encounter with the given type and date as the encounter datetime
+	 *
+	 * @param encounterType
+	 * @param date
+	 */
+	private void setEncounter(String encounterType, String date) {
+		encounter = new Encounter();
+		encounter.setEncounterType(encounterService.getEncounterType(encounterType));
+		encounter.setEncounterDatetime(makeDate(date));
+	}
 
-        // set up Context
-        PowerMockito.mockStatic(Context.class);
-        Mockito.when(Context.getConceptService()).thenReturn(conceptService);
-        Mockito.when(Context.getEncounterService()).thenReturn(encounterService);
+	/**
+	 * @verifies find out if a particular Obs is consumed
+	 * @see LostToFollowUpPatientSnapshot#consume(org.openmrs.Obs)
+	 */
 
-        rule = new LostToFollowUpPatientSnapshot();
+	@Test
+	public void consume_shouldProperlyDetermineTOfromAnObservation() throws Exception {
+		setEncounter(this.ENCOUNTER_TYPE_ADULT_INITIAL, "16 Oct 1975");
+		setObs(LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER, LostToFollowUpPatientSnapshot.CONCEPT_AMPATH, "16 Oct 1975");
+		rule.consume(currentObs);
+		assertThat(rule.getProperty("why").toString(), is("TO | (Ampath) "));
+		assertThat(rule.getProperty("obsDate").toString(), is("16-Oct-75"));
+	}
 
-    }
+	@Test
+	public void consume_shouldProperlyDetermineDEADfromAnEncounter() throws Exception {
+		setEncounter(this.ENCOUNTER_TYPE_DEATH_REPORT, "16 Oct 1975");
+		rule.consume(encounter);
+		assertThat(rule.getProperty("reason").toString(), is("DEAD | 16-Oct-75"));
+	}
 
-    /**
-     * generate a date from a string
-     *
-     * @param date
-     * @return
-     */
-    private Date makeDate(String date) {
-        try {
-            return new SimpleDateFormat("d MMM yyyy", Locale.ENGLISH).parse(date);
-        } catch (Exception e) {
-            // pass
-        }
-        return new Date();
-    }
-
-    /**
-     * adds an observation with the given date as the obs datetime
-     *
-     * @param concept
-     * @param date
-     */
-    private void addObs(String concept, String answer, String date) {
-        currentObs = new Obs();
-        currentObs.setConcept(conceptService.getConcept(concept));
-        currentObs.setValueCoded(conceptService.getConcept(answer));
-        currentObs.setObsDatetime(makeDate(date));
-        currentObs.setEncounter(encounter);
-       // currentObs.add(obs);
-    }
-
-    /**
-     * adds an encounter with the given type and date as the encounter datetime
-     *
-     * @param encounterType
-     * @param date
-     */
-    private void addEncounter(String encounterType, String date) {
-        encounter  = new Encounter();
-        encounter.setEncounterType(encounterService.getEncounterType(encounterType));
-        encounter.setEncounterDatetime(makeDate(date));
-
-    }
-
-    /**
-     * @verifies find out if a particular Obs is consumed
-     * @see LostToFollowUpPatientSnapshot#consume(org.openmrs.Obs)
-     */
-
-    @Test
-    public void consume_shouldProperlyDetermineTOfromAnObservation() throws Exception {
-        addEncounter(this.ENCOUNTER_TYPE_ADULT_INITIAL,"16 Oct 1975" );
-        addObs(LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER, LostToFollowUpPatientSnapshot.CONCEPT_AMPATH, "16 Oct 1975") ;
-        Assert.assertNotNull("A null encounter was encountered", encounter);
-        Assert.assertNotNull("A null Obs was encountered", currentObs);
-        rule.consume(currentObs);
-
-        String expectedRes = "TO | (Ampath) 16-Oct-1975";
-
-        Assert.assertEquals("They are not equal",expectedRes,rule.getProperty("reason"));
-
-
-    }
-
-    @Test
-    public void consume_shouldProperlyDetermineDEADfromAnEncounter() throws Exception {
-        addEncounter(this.ENCOUNTER_TYPE_DEATH_REPORT,"16 Oct 1975" );
-
-        Assert.assertNotNull("A null encounter was encountered", encounter);
-        rule.consume(encounter);
-
-        String expectedRes = "DEAD | 16-Oct-1975";
-
-        Assert.assertEquals("They are not equal",expectedRes,rule.getProperty("reason"));
-
-
-    }
-
-    @Test
-    public void consume_shouldProperlyDetermineLTFUfromAnEncounter() throws Exception {
-        addEncounter(this.ENCOUNTER_TYPE_ADULT_INITIAL,"16 Oct 2012" );
-
-        Assert.assertNotNull("A null encounter was encountered", encounter);
-
-        rule.consume(encounter);
-
-        String expectedRes = "LTFU | 16-Oct-2012";
-
-        Assert.assertEquals("They are not equal",expectedRes,rule.getProperty("reason"));
-
-
-    }
+	@Test
+	public void consume_shouldProperlyDetermineLTFUfromAnEncounter() throws Exception {
+		setEncounter(this.ENCOUNTER_TYPE_ADULT_INITIAL, "16 Oct 2012");
+		rule.consume(encounter);
+		assertThat(rule.getProperty("reason").toString(), is("LTFU | 16-Oct-12"));
+	}
 
 
 }
--- api/src/test/java/org/openmrs/module/amrsreport/rule/collection/MohLostToFollowUpRuleTest.java
+++ api/src/test/java/org/openmrs/module/amrsreport/rule/collection/MohLostToFollowUpRuleTest.java
@@ -5,7 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.openmrs.*;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.EncounterType;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.PatientService;
@@ -19,7 +23,12 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * A test file for MohLostToFollowUpRule class.
@@ -28,176 +37,176 @@ import java.util.*;
 @PrepareForTest(Context.class)
 public class MohLostToFollowUpRuleTest {
 
-    private static final List<String> initConcepts = Arrays.asList(
-            LostToFollowUpPatientSnapshot.CONCEPT_DATE_OF_DEATH,
-            LostToFollowUpPatientSnapshot.CONCEPT_DECEASED,
-            LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER,
-            LostToFollowUpPatientSnapshot.CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE,
-            LostToFollowUpPatientSnapshot.CONCEPT_AMPATH,
-            MohEvaluableNameConstants.RETURN_VISIT_DATE
+	private static final List<String> initConcepts = Arrays.asList(
+			LostToFollowUpPatientSnapshot.CONCEPT_DATE_OF_DEATH,
+			LostToFollowUpPatientSnapshot.CONCEPT_DECEASED,
+			LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER,
+			LostToFollowUpPatientSnapshot.CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE,
+			LostToFollowUpPatientSnapshot.CONCEPT_AMPATH,
+			MohEvaluableNameConstants.RETURN_VISIT_DATE
 
-    );
-    private String ENCOUNTER_TYPE_ADULT_INITIAL="ADULTINITIAL";
-    private String ENCOUNTER_TYPE_ADULT_RETURN="ADULTRETURN";
-    private String ENCOUNTER_TYPE_DEATH_REPORT="DEATHREPORT";
-
-
-    private static final int PATIENT_ID = 5;
-    private Patient patient;
-    private PatientService patientService;
-    private ConceptService conceptService;
-    private MohCoreService mohCoreService;
-    private EncounterService encounterService;
-    private MohLostToFollowUpRule rule;
-    private List<Obs> currentObs;
-    private Encounter encounter;
-
-    @Before
-    public void setup() {
-
-        currentObs = new ArrayList<Obs>();
-        // build the patient
-        patient = new Patient();
-
-        // build the mock patient service
-        patientService = Mockito.mock(PatientService.class);
-        Mockito.when(patientService.getPatient(PATIENT_ID)).thenReturn(patient);
-
-        // build the concept service
-        int i = 0;
-        conceptService = Mockito.mock(ConceptService.class);
-        for (String conceptName : initConcepts) {
-            Mockito.when(conceptService.getConcept(conceptName)).thenReturn(new Concept(i++));
-        }
-
-        // build the mock  services
-        encounterService = Mockito.mock(EncounterService.class);
-
-        Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_INITIAL)).thenReturn(new EncounterType(0));
-        Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_RETURN)).thenReturn(new EncounterType(1));
-        Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_DEATH_REPORT)).thenReturn(new EncounterType(2));
-
-        // build the MOH Core service
-        mohCoreService = Mockito.mock(MohCoreService.class);
-        Mockito.when(mohCoreService.getPatientObservations(Mockito.eq(PATIENT_ID),
-            Mockito.anyMap(), Mockito.any(MohFetchRestriction.class))).thenReturn(currentObs);
-
-        // set up Context
-        PowerMockito.mockStatic(Context.class);
-        Mockito.when(Context.getConceptService()).thenReturn(conceptService);
-        Mockito.when(Context.getService(MohCoreService.class)).thenReturn(mohCoreService);
-        Mockito.when(Context.getPatientService()).thenReturn(patientService);
-        Mockito.when(Context.getEncounterService()).thenReturn(encounterService);
-
-        // create a rule instance
-        rule = new MohLostToFollowUpRule();
+	);
+	private String ENCOUNTER_TYPE_ADULT_INITIAL = "ADULTINITIAL";
+	private String ENCOUNTER_TYPE_ADULT_RETURN = "ADULTRETURN";
+	private String ENCOUNTER_TYPE_DEATH_REPORT = "DEATHREPORT";
 
 
-    }
+	private static final int PATIENT_ID = 5;
+	private Patient patient;
+	private PatientService patientService;
+	private ConceptService conceptService;
+	private MohCoreService mohCoreService;
+	private EncounterService encounterService;
+	private MohLostToFollowUpRule rule;
+	private List<Obs> currentObs;
+	private Encounter encounter;
 
-    /**
-     * generate a date from a string
-     *
-     * @param date
-     * @return
-     */
-    private Date makeDate(String date) {
-        try {
-            return new SimpleDateFormat("d MMM yyyy", Locale.ENGLISH).parse(date);
-        } catch (Exception e) {
-            // pass
-        }
-        return new Date();
-    }
+	@Before
+	public void setup() {
 
-    /**
-     * adds an observation with the given date as the obs datetime
-     *
-     * @param concept
-     * @param date
-     */
-    private void addObs(String concept, String answer, String date) {
-        Obs obs = new Obs();
-        obs.setConcept(conceptService.getConcept(concept));
-        obs.setValueCoded(conceptService.getConcept(answer));
-        obs.setObsDatetime(makeDate(date));
-        currentObs.add(obs);
-    }
+		currentObs = new ArrayList<Obs>();
+		// build the patient
+		patient = new Patient();
 
-    /**
-     * adds a stop observation with the given date as the obs datetime
-     *
-     * @param conceptName
-     * @param date
-     */
-    private void addObsValueDateTime(String concept,String valueDatetime,String date) {
-        Obs obs = new Obs();
-        obs.setConcept(conceptService.getConcept(concept));
-        obs.setValueDatetime(makeDate(valueDatetime));
-        obs.setObsDatetime(makeDate(date));
-        currentObs.add(obs);
-    }
+		// build the mock patient service
+		patientService = Mockito.mock(PatientService.class);
+		Mockito.when(patientService.getPatient(PATIENT_ID)).thenReturn(patient);
 
-    /**
-     * adds an encounter with the given type and date as the encounter datetime
-     *
-     * @param encounterType
-     * @param date
-     */
-    private void addEncounter(String encounterType, String date) {
-        encounter  = new Encounter();
-        encounter.setEncounterType(encounterService.getEncounterType(encounterType));
-        encounter.setEncounterDatetime(makeDate(date));
+		// build the concept service
+		int i = 0;
+		conceptService = Mockito.mock(ConceptService.class);
+		for (String conceptName : initConcepts) {
+			Mockito.when(conceptService.getConcept(conceptName)).thenReturn(new Concept(i++));
+		}
 
-    }
+		// build the mock  services
+		encounterService = Mockito.mock(EncounterService.class);
+
+		Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_INITIAL)).thenReturn(new EncounterType(0));
+		Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_ADULT_RETURN)).thenReturn(new EncounterType(1));
+		Mockito.when(encounterService.getEncounterType(this.ENCOUNTER_TYPE_DEATH_REPORT)).thenReturn(new EncounterType(2));
+
+		// build the MOH Core service
+		mohCoreService = Mockito.mock(MohCoreService.class);
+		Mockito.when(mohCoreService.getPatientObservations(Mockito.eq(PATIENT_ID),
+				Mockito.anyMap(), Mockito.any(MohFetchRestriction.class))).thenReturn(currentObs);
+
+		// set up Context
+		PowerMockito.mockStatic(Context.class);
+		Mockito.when(Context.getConceptService()).thenReturn(conceptService);
+		Mockito.when(Context.getService(MohCoreService.class)).thenReturn(mohCoreService);
+		Mockito.when(Context.getPatientService()).thenReturn(patientService);
+		Mockito.when(Context.getEncounterService()).thenReturn(encounterService);
+
+		// create a rule instance
+		rule = new MohLostToFollowUpRule();
 
 
-    @Test
-    public void consume_shouldProperlyDetermineDEADfromAnEncounter() throws Exception {
-        Calendar calendar= Calendar.getInstance();
-        calendar.set(2012,Calendar.DECEMBER, 20);
+	}
 
-        Date deathDate = calendar.getTime();
-        patient.setDead(true);
-        patient.setDeathDate(deathDate);
+	/**
+	 * generate a date from a string
+	 *
+	 * @param date
+	 * @return
+	 */
+	private Date makeDate(String date) {
+		try {
+			return new SimpleDateFormat("d MMM yyyy", Locale.ENGLISH).parse(date);
+		} catch (Exception e) {
+			// pass
+		}
+		return new Date();
+	}
 
-        Assert.assertTrue("The Patient is alive",patient.getDead());
-        Assert.assertEquals("Result for DEAD not correct",new Result("DEAD | 20-Dec-2012"), rule.evaluate(null,PATIENT_ID, null));
+	/**
+	 * adds an observation with the given date as the obs datetime
+	 *
+	 * @param concept
+	 * @param date
+	 */
+	private void addObs(String concept, String answer, String date) {
+		Obs obs = new Obs();
+		obs.setConcept(conceptService.getConcept(concept));
+		obs.setValueCoded(conceptService.getConcept(answer));
+		obs.setObsDatetime(makeDate(date));
+		currentObs.add(obs);
+	}
 
-    }
+	/**
+	 * adds a stop observation with the given date as the obs datetime
+	 *
+	 * @param conceptName
+	 * @param date
+	 */
+	private void addObsValueDateTime(String concept, String valueDatetime, String date) {
+		Obs obs = new Obs();
+		obs.setConcept(conceptService.getConcept(concept));
+		obs.setValueDatetime(makeDate(valueDatetime));
+		obs.setObsDatetime(makeDate(date));
+		currentObs.add(obs);
+	}
 
-    @Test
-    public void consume_shouldProperlyDetermineTOfromAnObservation() throws Exception {
+	/**
+	 * adds an encounter with the given type and date as the encounter datetime
+	 *
+	 * @param encounterType
+	 * @param date
+	 */
+	private void addEncounter(String encounterType, String date) {
+		encounter = new Encounter();
+		encounter.setEncounterType(encounterService.getEncounterType(encounterType));
+		encounter.setEncounterDatetime(makeDate(date));
 
-        addObs(LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER, LostToFollowUpPatientSnapshot.CONCEPT_AMPATH, "16 Oct 1975") ;
+	}
 
-        Assert.assertEquals("current Obs size is wrong!", 1, currentObs.size());
 
-        Assert.assertEquals("Test for TO is wrong ",new Result("TO | (Ampath) 16-Oct-1975"),rule.evaluate(null,PATIENT_ID,null));
-    }
+	@Test
+	public void consume_shouldProperlyDetermineDEADfromAnEncounter() throws Exception {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, Calendar.DECEMBER, 20);
 
-    @Test
-    public void consume_shouldProperlyDetermineLTFUfromAnObsUsingCONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE() throws Exception {
+		Date deathDate = calendar.getTime();
+		patient.setDead(true);
+		patient.setDeathDate(deathDate);
 
-        addObsValueDateTime(LostToFollowUpPatientSnapshot.CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE,"25 Aug 2012","16 Aug 2012");
+		Assert.assertTrue("The Patient is alive", patient.getDead());
+		Assert.assertEquals("Result for DEAD not correct", new Result("DEAD | 20-Dec-12"), rule.evaluate(null, PATIENT_ID, null));
 
-        Assert.assertNotNull("A null Obs was encountered", currentObs);
-        Assert.assertEquals("Returned wrong number for Obs",1,currentObs.size());
+	}
 
-        Assert.assertEquals("Result for LFTU using CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE tested negative",new Result("LTFU | 25-Aug-2012"),rule.evaluate(null,PATIENT_ID, null));
+	@Test
+	public void consume_shouldProperlyDetermineTOfromAnObservation() throws Exception {
 
-    }
+		addObs(LostToFollowUpPatientSnapshot.CONCEPT_TRANSFER_CARE_TO_OTHER_CENTER, LostToFollowUpPatientSnapshot.CONCEPT_AMPATH, "16 Oct 1975");
 
-    @Test
-    public void consume_shouldProperlyDetermineLTFUfromAnObsUsingRETURN_VISIT_DATE() throws Exception {
+		Assert.assertEquals("current Obs size is wrong!", 1, currentObs.size());
 
-        addObsValueDateTime(MohEvaluableNameConstants.RETURN_VISIT_DATE,"16 Oct 2012","16 Aug 2012");
+		Assert.assertEquals("Test for TO is wrong ", new Result("TO | (Ampath) 16-Oct-75"), rule.evaluate(null, PATIENT_ID, null));
+	}
 
-        Assert.assertNotNull("A null Obs was encountered", currentObs);
-        Assert.assertEquals("Returned wrong number for Obs",1,currentObs.size());
+	@Test
+	public void consume_shouldProperlyDetermineLTFUfromAnObsUsingCONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE() throws Exception {
 
-        Assert.assertEquals("Result for LFTU using RETURN_VISIT_DATE tested negative",new Result("LTFU | 16 Oct 2012"),rule.evaluate(null,PATIENT_ID, null));
+		addObsValueDateTime(LostToFollowUpPatientSnapshot.CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE, "25 Aug 2012", "16 Aug 2012");
 
-    }
+		Assert.assertNotNull("A null Obs was encountered", currentObs);
+		Assert.assertEquals("Returned wrong number for Obs", 1, currentObs.size());
+
+		Assert.assertEquals("Result for LFTU using CONCEPT_RETURN_VISIT_DATE_EXP_CARE_NURSE tested negative", new Result("LTFU | 25-Aug-12"), rule.evaluate(null, PATIENT_ID, null));
+
+	}
+
+	@Test
+	public void consume_shouldProperlyDetermineLTFUfromAnObsUsingRETURN_VISIT_DATE() throws Exception {
+
+		addObsValueDateTime(MohEvaluableNameConstants.RETURN_VISIT_DATE, "16 Oct 2012", "16 Aug 2012");
+
+		Assert.assertNotNull("A null Obs was encountered", currentObs);
+		Assert.assertEquals("Returned wrong number for Obs", 1, currentObs.size());
+
+		Assert.assertEquals("Result for LFTU using RETURN_VISIT_DATE tested negative", new Result("LTFU | 16-Oct-12"), rule.evaluate(null, PATIENT_ID, null));
+
+	}
 
 }
