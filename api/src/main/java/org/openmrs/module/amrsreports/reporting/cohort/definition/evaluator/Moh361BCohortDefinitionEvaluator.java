4f637e3ee6a4f271e5d52b1383de0a14032fa132
ojwanganto <ojwangwachiaje@gmail.com>
2013-06-28 15:18:16 +0300

    Added transfer Ins to cohort definition

--- api/src/main/java/org/openmrs/module/amrsreports/reporting/cohort/definition/evaluator/Moh361BCohortDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/amrsreports/reporting/cohort/definition/evaluator/Moh361BCohortDefinitionEvaluator.java
@@ -36,7 +36,7 @@ public class Moh361BCohortDefinitionEvaluator implements CohortDefinitionEvaluat
             return null;
 
         String reportDate = sdf.format(context.getEvaluationDate());
-        List<Location> locationList = (List<Location>) context.getParameterValue("locationList");
+
 
 
         String sql =
@@ -47,6 +47,31 @@ public class Moh361BCohortDefinitionEvaluator implements CohortDefinitionEvaluat
                         "  and enrollment_date is not NULL" +
                         "  and first_arv_date <= ':reportDate'" +
                         "  and first_arv_location_id in ( :locationList )";
+
+        /*add transfer ins*/
+        List<Location> locationList = (List<Location>) context.getParameterValue("locationList");
+        for (Location location : locationList) {
+            String personAttributeQuery =
+                    " union " +
+                            " select pa.person_id" +
+                            " from person_attribute pa join amrsreports_hiv_care_enrollment ae" +
+                            "     on pa.person_id = ae.person_id" +
+                            "       and ae.enrollment_date is not null" +
+                            "       and ae.enrollment_date <= ':reportDate'" +
+                            "   join encounter e " +
+                            "     on e.patient_id = pa.person_id" +
+                            "       and e.voided = 0" +
+                            "       and e.location_id in ( :locationList )" +
+                            " where (pa.voided = 0" +
+                            "        or (pa.voided = 1 and pa.void_reason like 'New value: %'))" +
+                            "   and pa.person_attribute_type_id = 7" +
+                            "   and pa.value = '" + location.getLocationId() + "'" +
+                            "   and pa.date_created <= ':reportDate'";
+
+            sql = sql + personAttributeQuery;
+        }
+
+
 
         SqlCohortDefinition sqlCohortDefinition = new SqlCohortDefinition(sql.replaceAll(":reportDate", reportDate));
         Cohort results = Context.getService(CohortDefinitionService.class).evaluate(sqlCohortDefinition, context);
