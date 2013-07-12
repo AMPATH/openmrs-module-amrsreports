92c989899c7f35402f829f9cecfab729484a426f
Jeremy Keiper <jeremy@openmrs.org>
2013-07-12 11:33:58 -0400

    REPORTS-153 - added episode to pregnancy table

--- api/src/main/java/org/openmrs/module/amrsreports/builder/PregnancyTableBuilder.java
+++ api/src/main/java/org/openmrs/module/amrsreports/builder/PregnancyTableBuilder.java
@@ -15,7 +15,13 @@
 package org.openmrs.module.amrsreports.builder;
 
 import org.apache.commons.lang.StringUtils;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.amrsreports.util.TableBuilderUtil;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class PregnancyTableBuilder {
 
@@ -45,6 +51,7 @@ public class PregnancyTableBuilder {
 					"  `edd_edc` datetime DEFAULT NULL," +
 					"  `edd_wkmn` datetime DEFAULT NULL," +
 					"  `edd_lmp` datetime DEFAULT NULL," +
+					"  `episode` int DEFAULT 0," +
 					"  `pregstatus` int(1) DEFAULT 0," +
 					"  `probpreg` int(1) DEFAULT 0," +
 					"  `testpreg` int(1) DEFAULT 0," +
@@ -58,7 +65,6 @@ public class PregnancyTableBuilder {
 					"  PRIMARY KEY (`pregnancy_id`)," +
 					"  KEY `amrsreport_pregnancy_pregdate` (`pregnancy_date`)," +
 					"  CONSTRAINT `amrsreport_unique_person_pregdate` UNIQUE (`person_id`, `pregnancy_date`)" +
-//					"  CONSTRAINT `amrsreport_unique_person_pregdate` UNIQUE (`person_id`, `pregnancy_date`)," +
 //					"  CONSTRAINT `amrsreport_pregnancy_person_ref` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`)" +
 					") ENGINE=InnoDB DEFAULT CHARSET=utf8";
 
@@ -150,11 +156,13 @@ public class PregnancyTableBuilder {
 			"INSERT INTO amrsreports_pregnancy (" +
 					"	person_id," +
 					"	pregnancy_date," +
-					"	:source" +
+					"	:source," +
+					"   :column" +
 					" ) SELECT" +
 					"	e.patient_id, " +
 					"	DATE_FORMAT(e.encounter_datetime, '%Y-%m-%d') AS p_date," +
-					"	DATE_ADD(o.obs_datetime, INTERVAL (280 - (o.value_numeric * :days)) DAY) AS d_date" +
+					"	DATE_ADD(o.obs_datetime, INTERVAL (280 - (o.value_numeric * :days)) DAY) AS d_date," +
+					"   1" +
 					" FROM" +
 					"	obs o INNER JOIN encounter e ON e.encounter_id = o.encounter_id AND e.voided = 0" +
 					" WHERE" +
@@ -165,7 +173,8 @@ public class PregnancyTableBuilder {
 					" ORDER BY" +
 					"	p_date asc" +
 					" ON DUPLICATE KEY UPDATE" +
-					"	:source = VALUES(due_date)";
+					"	:source = VALUES(due_date)," +
+					"   :column = 1";
 
 	private static final String UPDATE_EDD_PREFERENCE =
 			"UPDATE amrsreports_pregnancy" +
@@ -180,10 +189,30 @@ public class PregnancyTableBuilder {
 					"       IF(edd_wkmn IS NOT NULL, 'GEST'," +
 					"         IF(edd_fh IS NOT NULL, 'FH', NULL))))";
 
+	private static final String DISTINCT_PEOPLE_WITH_DUE_DATES =
+			"select distinct person_id" +
+					" from amrsreports_pregnancy" +
+					" where due_date is not null";
+
+	private static String EDDS_FOR_PERSON =
+			"select pregnancy_id, pregnancy_date, due_date" +
+					" from amrsreports_pregnancy" +
+					" where person_id = :personId" +
+					"   and due_date is not null" +
+					" order by pregnancy_date asc";
+
+	private static String MACRO_UPDATE_EPISODE =
+			"update amrsreports_pregnancy" +
+					" SET episode = :episode" +
+					" WHERE pregnancy_id = :rowId";
+
+	private static final Integer ACCEPTABLE_EDD_GAP = 30;
+
 	/**
 	 * drops, creates and fills out the pregnancy table
 	 */
 	public void execute() {
+		AdministrationService admin = Context.getAdministrationService();
 
 		// drop the table
 		TableBuilderUtil.runUpdateSQL(DROP_TABLE);
@@ -196,28 +225,29 @@ public class PregnancyTableBuilder {
 		updateColumn("probpreg", "concept_id in (6042, 1790) and value_coded in (44, 47, 46)");
 		updateColumn("testpreg", "(concept_id = 45 and value_coded = 703) or (concept_id = 1856 and value_coded <> 1175)");
 		updateColumn("reasnvispreg", "concept_id in (1834, 1835) and value_coded = 1831");
-		// TODO add setting fundpreg and durpreg columns to 1 in updateEDD
-		updateColumn("durpreg", "concept_id in (1279, 5992) and value_numeric > 0");
-		updateColumn("fundpreg", "concept_id = 1855 and value_numeric > 0");
 		updateColumn("ancpreg", "concept_id = 2055 and value_coded = 1065");
 		updateColumn("eddpreg", "concept_id in (5596, 1854) and value_coded > obs_datetime");
 		updateColumn("arvpreg", "(concept_id = 1181 and value_coded = 1148)" +
 				" or (concept_id = 1251 and value_coded = 1776)" +
 				" or (concept_id = 1992 and value_coded not in (1066, 67))");
 
+		// ***** fundpreg and durpreg handled in updateEDD
+		// updateColumn("durpreg", "concept_id in (1279, 5992) and value_numeric > 0");
+		// updateColumn("fundpreg", "concept_id = 1855 and value_numeric > 0");
+
 		// update due dates based on obsDatetime and valueNumeric
 
 		// obs date + (280 days - # days in weeks gestation from fundal height)
-		updateEDD("edd_fh", 7, "concept_id = 1855");
+		updateEDD("edd_fh", 7, "concept_id = 1855", "fundpreg");
 
 		// obs date + (280 days - # days in months gestation)
-		updateEDD("edd_wkmn", 30, "concept_id = 5992 AND value_numeric <= 9");
+		updateEDD("edd_wkmn", 30, "concept_id = 5992 AND value_numeric <= 9", "durpreg");
 
 		// obs date + (280 days - # days in weeks gestation) ... value over 9 is considered to mean weeks
-		updateEDD("edd_wkmn", 7, "concept_id = 5992 AND value_numeric > 9");
+		updateEDD("edd_wkmn", 7, "concept_id = 5992 AND value_numeric > 9", "durpreg");
 
 		// obs date + (280 days - # days in weeks pregnant)
-		updateEDD("edd_wkmn", 7, "concept_id = 1279");
+		updateEDD("edd_wkmn", 7, "concept_id = 1279", "durpreg");
 
 		// update due dates based on valueDatetime
 
@@ -229,6 +259,55 @@ public class PregnancyTableBuilder {
 
 		// select EDD based on priorities
 		TableBuilderUtil.runUpdateSQL(UPDATE_EDD_PREFERENCE);
+
+		// get unique people
+		List<List<Object>> people = admin.executeSQL(DISTINCT_PEOPLE_WITH_DUE_DATES, true);
+
+		// loop through them
+		for (List<Object> person : people) {
+			Integer personId = (Integer) person.get(0);
+
+			// get EDDs
+			List<List<Object>> edds = admin.executeSQL(
+					EDDS_FOR_PERSON.replaceAll(":personId", personId.toString()), true);
+
+			// set a few variables
+			Integer episode = 0;
+			Date benchmark = null;
+
+			// loop through EDDs
+			for (List<Object> row : edds) {
+
+				Integer rowId = (Integer) row.get(0);
+				Date edd = (Date) row.get(2);
+
+				// if the benchmark has not been set yet (first time)
+				// or the edd is after the specified benchmark,
+				// start a new episode.
+				if (benchmark == null || edd.after(benchmark)) {
+					Date encDate = (Date) row.get(1);
+					benchmark = addWeeksToDate(encDate, 40);
+					episode++;
+				}
+
+				// 87158
+				// 513336
+
+				// update this row with the episode number
+				updateEpisode(rowId, episode);
+			}
+		}
+	}
+
+	private void updateEpisode(Integer rowId, Integer episode) {
+		if (rowId == null || episode == null)
+			return;
+
+		String query = MACRO_UPDATE_EPISODE
+				.replaceAll(":rowId", rowId.toString())
+				.replaceAll(":episode", episode.toString());
+
+		TableBuilderUtil.runUpdateSQL(query);
 	}
 
 	/**
@@ -238,23 +317,37 @@ public class PregnancyTableBuilder {
 		if (StringUtils.isBlank(column) || StringUtils.isBlank(criteria))
 			return;
 
-		String query = MACRO_UPDATE_COLUMN.replaceAll(":criteria", criteria).replaceAll(":column", column);
+		String query = MACRO_UPDATE_COLUMN
+				.replaceAll(":criteria", criteria)
+				.replaceAll(":column", column);
+
 		TableBuilderUtil.runUpdateSQL(query);
 	}
 
 	/**
 	 * updates due_date based on provided criteria
 	 */
-	private void updateEDD(String source, Integer days, String criteria) {
+	private void updateEDD(String source, Integer days, String criteria, String column) {
 		if (days == null || StringUtils.isBlank(criteria))
 			return;
 
 		String query = MACRO_UPDATE_EDD
 				.replaceAll(":source", source)
 				.replaceAll(":criteria", criteria)
-				.replaceAll(":days", days.toString());
+				.replaceAll(":days", days.toString())
+				.replaceAll(":column", column);
 
 		TableBuilderUtil.runUpdateSQL(query);
+	}
+
+	/**
+	 * adds a number of days to a given date
+	 */
+	private Date addWeeksToDate(Date date, Integer weeks) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		c.add(Calendar.WEEK_OF_MONTH, weeks);
+		return c.getTime();
 	}
 
 }
