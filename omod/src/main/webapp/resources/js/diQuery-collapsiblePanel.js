6eec6a4a0bf7dcf42d161a1dfc0d4ad9b1f76d20
Jeremy Keiper <jeremy@openmrs.org>
2013-07-19 12:24:05 -0400

    REPORTS-197 - final css and rendering touchups

--- omod/src/main/webapp/resources/js/diQuery-collapsiblePanel.js
+++ omod/src/main/webapp/resources/js/diQuery-collapsiblePanel.js
@@ -12,23 +12,28 @@
         };
         var options = $j.extend(defaults, options);
 
-        $j(this).click(function () {
+        $j(this).click(function (event) {
+            event.preventDefault();
+
             // optionally add the class .toggleDiv to each div you want to automatically close
             $j('.toggleDiv:hidden').slideUp(options.speed, options.easing);
+
             // this var stores which button you've clicked
             var toggleClick = $j(this);
+
             // this reads the rel attribute of the button to determine which div id to toggle
             var toggleDiv = $j(this).attr('rel');
+
             // here we toggle show/hide the correct div at the right speed and using which easing effect
             $j(toggleDiv).slideToggle(options.speed, options.easing, function () {
+
+                console.log("ToggleClick = " + toggleClick);
+
                 // this only fires once the animation is completed
                 if (options.changeText == 1) {
-                    $j(toggleDiv).is(":visible") ? toggleClick.value(options.hideText) : toggleClick.value(options.showText);
+                    $j(toggleDiv).is(":visible") ? toggleClick.text(options.hideText) : toggleClick.text(options.showText);
                 }
             });
-
-            return false;
-
         });
 
     };
