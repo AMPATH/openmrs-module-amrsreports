f4607cc5f425340397d49557c1a761fc3ef0948b
ojwanganto <ojwangantony@yahoo.com>
2014-01-08 12:54:39 +0300

    REPORTS-214: Month Zero for ArtStartDate

--- api/src/main/java/org/openmrs/module/amrsreports/reporting/provider/MOH361BReportProvider_0_1.java
+++ api/src/main/java/org/openmrs/module/amrsreports/reporting/provider/MOH361BReportProvider_0_1.java
@@ -9,11 +9,7 @@ import org.openmrs.module.amrsreports.AmrsReportsConceptNames;
 import org.openmrs.module.amrsreports.AmrsReportsConstants;
 import org.openmrs.module.amrsreports.cache.MohCacheUtils;
 import org.openmrs.module.amrsreports.reporting.cohort.definition.Moh361BCohortDefinition;
-import org.openmrs.module.amrsreports.reporting.converter.DateListCustomConverter;
-import org.openmrs.module.amrsreports.reporting.converter.DecimalAgeConverter;
-import org.openmrs.module.amrsreports.reporting.converter.IntervalObsValueNumericConverter;
-import org.openmrs.module.amrsreports.reporting.converter.ObsRepresentationValueNumericConverter;
-import org.openmrs.module.amrsreports.reporting.converter.WHOStageConverter;
+import org.openmrs.module.amrsreports.reporting.converter.*;
 import org.openmrs.module.amrsreports.reporting.data.AgeAtEvaluationDateDataDefinition;
 import org.openmrs.module.amrsreports.reporting.data.CohortRestrictedBirthdateDataDefinition;
 import org.openmrs.module.amrsreports.reporting.data.CohortRestrictedGenderDataDefinition;
@@ -161,6 +157,9 @@ public class MOH361BReportProvider_0_1 extends ReportProvider {
 		MappedData<DateARTStartedDataDefinition> artDateMap = new MappedData<DateARTStartedDataDefinition>();
 		artDateMap.setParameterizable(dateARTStarted);
 		artDateMap.addConverter(new DateConverter());
+
+        //aa. month zero
+        dsd.addColumn("Month 0 (Zero)",new DateARTStartedDataDefinition(),nullString,new ARTMonthZeroConverter());
 
 		// ah. 6 month CD4 count (and aq, az, bi)
 		SortedObsSinceOtherDefinitionDataDefinition sixMonthCD4 = new SortedObsSinceOtherDefinitionDataDefinition("6 Month CD4");
---
+++ api/src/test/java/org/openmrs/module/amrsreports/reporting/converter/ARTMonthZeroConverterTest.java
@@ -0,0 +1,32 @@
+package org.openmrs.module.amrsreports.reporting.converter;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * test class for ARTMonthZeroConverter class
+ */
+public class ARTMonthZeroConverterTest {
+    @Test
+    public void shouldReturnDecember() {
+        Date dateOne = new Date("05 Dec 2012");
+
+        ARTMonthZeroConverter artMonthZeroConverter = new ARTMonthZeroConverter();
+
+        Assert.assertEquals("December", artMonthZeroConverter.convert(dateOne));
+
+    }
+
+    @Test
+    public void testConvert() {
+        Date dateOne = new Date("12/1/2012");
+
+        ARTMonthZeroConverter artMonthZeroConverter = new ARTMonthZeroConverter();
+
+        Assert.assertEquals("December", artMonthZeroConverter.convert(dateOne));
+
+    }
+}
