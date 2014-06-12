a28b0ab91198ba5e247acdac45d60dcb3e2b173f
mhawila <a.mhawila@gmail.com>
2014-06-12 15:36:45 -0400

    Modified NASCOP771 to extend AMRSReportCohortDefinition instead of BaseCohortDefinition to have consistency

--- api/src/main/java/org/openmrs/module/amrsreports/reporting/cohort/definition/NASCOP771CohortDefinition.java
+++ api/src/main/java/org/openmrs/module/amrsreports/reporting/cohort/definition/NASCOP771CohortDefinition.java
@@ -1,9 +1,6 @@
 package org.openmrs.module.amrsreports.reporting.cohort.definition;
 
-import org.openmrs.module.amrsreports.MOHFacility;
-import org.openmrs.module.reporting.cohort.definition.BaseCohortDefinition;
 import org.openmrs.module.reporting.common.Localized;
-import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
 import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyCachingStrategy;
 import org.openmrs.module.reporting.evaluation.caching.Caching;
 
@@ -12,25 +9,6 @@ import org.openmrs.module.reporting.evaluation.caching.Caching;
  */
 @Caching(strategy = ConfigurationPropertyCachingStrategy.class)
 @Localized("reporting.NASCOP771CohortDefinition")
-public class NASCOP771CohortDefinition extends BaseCohortDefinition {
+public class NASCOP771CohortDefinition extends AMRSReportsCohortDefinition {
 
-	@ConfigurationProperty
-	private MOHFacility facility;
-
-	public NASCOP771CohortDefinition() {
-		super();
-	}
-
-	public NASCOP771CohortDefinition(MOHFacility facility) {
-		super();
-		this.facility = facility;
-	}
-
-	public MOHFacility getFacility() {
-		return facility;
-	}
-
-	public void setFacility(MOHFacility facility) {
-		this.facility = facility;
-	}
 }
